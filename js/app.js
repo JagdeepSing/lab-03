be9f4b3f61c5a519562cc8bad9095d84aee859f0
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-03-14 12:59:37 -0700

    styling left

--- js/app.js
+++ js/app.js
@@ -25,7 +25,6 @@ $(function() {
   Picture.prototype.toHtml = function() {
     let $template = $('#photo-template').html();
     let compiledTemplate = Handlebars.compile($template);
-    console.log($template);
     return compiledTemplate(this);
   };
 
@@ -60,23 +59,27 @@ $(function() {
   };
 
   Picture.loadPictures = (filePath) => {
+    Picture.all[filePath] = Picture.all[filePath].sort((a, b) => {
+      let sortType = $('.sortBtns:checked').val();
+      if (sortType === 'title') {
+        if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+        if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+        return 0;
+      } else if (sortType === 'horns') {
+        return b.horns - a.horns;
+      }
+    });
 
     Picture.all[filePath].forEach((pic) => {
-      console.log('loadPictures in');
-      console.log(pic.toHtml());
       $('#animal-wrap').append(pic.toHtml());
     });
-    // Array.sort(sortFunction)
-    //if title
-
   };
   //else this
 
-
   // filters animals by keyword
   let _filterImages = () => {
-  // on any change of the select dropdown list
-    $('filterList').on('change', () => {
+    // on any change of the select dropdown list
+    $('#filterList').on('change', () => {
       let selectedKeyword = $('select option:selected').val();
 
       // if elements are hidden and user selects default option in dropdown list,
@@ -84,9 +87,9 @@ $(function() {
       if (selectedKeyword === 'default') {
         $('.animal:hidden').show();
       } else {
-      // takes jQuery matched set and converts to an array and iterates over each HTML element
-      // and after converting each element back to a jQuery object (in order to use jQuery methods)
-      // filters objects for only those with the class of the selected keyword
+        // takes jQuery matched set and converts to an array and iterates over each HTML element
+        // and after converting each element back to a jQuery object (in order to use jQuery methods)
+        // filters objects for only those with the class of the selected keyword
         $('.animal')
           .toArray()
           .forEach((val) => {
@@ -102,48 +105,44 @@ $(function() {
   };
 
   let navBtns = () => {
-  // create HTML buttons
+    // create HTML buttons
     $('main').before(
-      '<button class="navButtons" id="0" type="button">Page 1</button>' +
-        '<button class="navButtons" id="1" type="button">Page 2</button>'
+      '<button class="navButtons" id="data/page-1.json" type="button">Page 1</button>' +
+        '<button class="navButtons" id="data/page-2.json" type="button">Page 2</button>'
     );
 
     // add event listeners to buttons
 
-    $('navButtons').on('click', () => {
-    // TODO:
+    $('.navButtons').click(function(event) {
+      // TODO:
+      currentPage = event.target.id;
+      $('.animal').remove();
+      Picture.loadPictures(currentPage);
     });
-  // hide current page and display clicked page
+    // hide current page and display clicked page
   };
 
   let sortBtns = () => {
     $('main').before(
-      '<button class="sortBtns" value="title" type="radio" checked> Sort by Title </button>' + '<button class="sortBtns" value="horns" type="radio"> Sort by Horns </button>'
-    )
+      '<input class="sortBtns" value="title" type="radio" name="sort" checked> Sort by Title </input>' +
+        '<input class="sortBtns" value="horns" name="sort" type="radio"> Sort by Horns </input>'
+    );
+    // add event lisitenters to buttons
+    $('.sortBtns').on('click', function() {
+      let checked = $('.sortBtns:checked').val();
+      if (sortedBy !== checked) {
+        sortedBy = checked;
+        $('.animal').remove();
+        Picture.loadPictures(currentPage);
+      }
+    });
+  };
 
-  }
-  // add event lisitenters to buttons
-  $('sortBtns').on('click', function() {
-
-    if( sortedBy !== this.val()){
-      sortedBy = this.val()
-      $('.animal').remove();
-      loadPictures(currentPage);
-    }
-
-  });
-
-  //reorder based on the sort type
-  //query all animals
-  //title
-  //horns
-
-
-  //_displayImages();
-  //_filterImages();
   $(() => {
     Picture.readJSON('data/page-1.json');
     Picture.readJSON('data/page-2.json');
+    _filterImages();
     navBtns();
+    sortBtns();
   });
 });
